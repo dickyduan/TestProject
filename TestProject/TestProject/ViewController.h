a4278f5ffb33dc8114fcd79d3015c869416bcf0d
dickyduan <duangl@hotmail.com>
2015-02-05 15:21:36 +0800

    upload testproject
    upload testproject

---
+++ TestProject/TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by duangl on 15/2/5.
+//  Copyright (c) 2015年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject/TestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestProject
+//
+//  Created by duangl on 15/2/5.
+//  Copyright (c) 2015年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
