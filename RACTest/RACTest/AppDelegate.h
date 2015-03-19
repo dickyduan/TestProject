55779a3293801aa40dda1bbb9060854481146cc6
dickyduan <duangl@hotmail.com>
2015-03-19 15:50:57 +0800

    pods and rac test

---
+++ RACTest/RACTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACTest
+//
+//  Created by duangl on 15/3/4.
+//  Copyright (c) 2015年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RACTest/RACTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RACTest
+//
+//  Created by duangl on 15/3/4.
+//  Copyright (c) 2015年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
